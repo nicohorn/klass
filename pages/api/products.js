f02c75be6a031ee84fba74d2eb50c23cf72e6923
Nicolás Horn <nicoskate000@gmail.com>
2022-11-20 23:48:20 -0300

    asdasdasd

---
+++ pages/api/products.js
@@ -0,0 +1,19 @@
+import clientPromise from "../../mongodb";
+
+export default async function handler(req, res) {
+  const client = await clientPromise;
+  const db = client.db("klass_ecommerce");
+  const collection = db.collection("products");
+
+  if (req.method === "GET") {
+    const findResult = await collection.find({}).toArray();
+    console.log("Found documents =>", findResult);
+    return res.status(200).json(findResult);
+  } else if (req.method === "POST") {
+    const insert = await collection.insertOne({ ...req.body });
+    const result = await collection.findOne({ _id: insert.insertedId });
+    console.log(result);
+
+    return "done";
+  }
+}
