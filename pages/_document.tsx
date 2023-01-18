158e1830decc1d8ec41c65149c7aa75bacf55474
Nicolás Horn <nicoskate000@gmail.com>
2023-01-18 16:45:18 -0300

    facebook pixel fixes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,6 +17,24 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/icon?family=Material+Icons"
             rel="stylesheet"
           ></link>
+          <Head>
+            {/* Facebook Pixel Code */}
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `!function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '908343633688942');
+                fbq('track', 'PageView');`,
+              }}
+            />
+            {/* End Facebook Pixel Code */}
+          </Head>
         </Head>
         <body className="overflow-visible overflow-y-scroll">
           <Main />
