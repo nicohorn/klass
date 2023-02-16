a084e3cf9bbedfc9c7ba7e983a9ccc44118ba714
Nicolás Horn <nicoskate000@gmail.com>
2023-02-16 05:33:46 -0300

    fixes

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -8,6 +8,7 @@ import Head from "next/head";
 var ObjectId = require("mongodb").ObjectId;
 
 import clientPromise from "../../mongodb";
+import ImageContainer from "pages/components/imagecontainer";
 
 export default function Id({ item }) {
   const addToCart = useProducts((state: any) => state.addToCart);
@@ -575,39 +576,12 @@ export default function Id({ item }) {
             )}
           </div>
         </div>
-        <div className="relative h-full group text-xl">
-          <img
-            id="productImage"
-            key={imageIndex}
-            className=" h-full object-cover  transition-all duration-200 object-center rounded-sm drop-shadow-[5px_5px_5px_rgba(0,0,0,0.20)]"
-            src={`${product.img[imageIndex]}`}
-            style={{ opacity: "0" }} // Set initial opacity to 0
-            onLoad={(e) => {
-              const img = e.target as HTMLImageElement; // Use type assertion to cast to HTMLImageElement
-              img.style.opacity = "1"; // Set opacity to 1 after image is loaded
-            }}
-          ></img>
-
-          <div
-            onClick={() => {
-              product.img.length - 1 === imageIndex
-                ? setImageIndex(0)
-                : setImageIndex(imageIndex + 1);
-            }}
-            className="heartbeat absolute transition-all duration-150 p-3 cursor-pointer active:scale-95 hover:scale-105 group-hover:opacity-100 opacity-30 rounded hover:bg-neutral-900 bg-neutral-900/70 text-white top-[45%] sm:right-5 right-2 "
-          >
-            {">"}
-          </div>
-          <div
-            onClick={() => {
-              imageIndex === 0
-                ? setImageIndex(product.img.length - 1)
-                : setImageIndex(imageIndex - 1);
-            }}
-            className="heartbeat absolute transition-all duration-150 cursor-pointer p-3 active:scale-95 hover:scale-105 group-hover:opacity-100 opacity-30 rounded hover:bg-neutral-900 bg-neutral-900/70 text-white top-[45%] sm:left-5 left-2"
-          >
-            {"<"}
-          </div>
+        <div className=" relative w-auto group text-xl ">
+          <ImageContainer
+            imageIndex={imageIndex}
+            img={product.img}
+            setImageIndex={setImageIndex}
+          ></ImageContainer>
         </div>
       </section>
     </>
