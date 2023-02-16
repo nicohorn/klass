8c0818206fd189950f3aca63c719d00b8116993d
Nicolás Horn <nicoskate000@gmail.com>
2023-02-16 10:26:46 -0300

    condition for image buttons
    added a condition for the navigation buttons of the product image container so that they only appear if there's more than one image

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -496,9 +496,13 @@ export default function Id({ item }) {
     return (
       <>
         <img
+          src={`${product.img[imageIndex + 1]}`}
+          className="absolute z-0 opacity-0 top-0 md:h-full h-auto object-cover transition-all duration-200 object-center rounded-sm"
+        ></img>
+        <img
           id="productImage"
           key={imageIndex}
-          className=" md:h-full h-auto object-cover transition-all duration-200 object-center rounded-sm drop-shadow-[5px_5px_5px_rgba(0,0,0,0.10)]"
+          className="z-30 md:h-full h-auto object-cover transition-all duration-200 object-center rounded-sm drop-shadow-[5px_5px_5px_rgba(0,0,0,0.10)]"
           src={`${product.img[imageIndex]}`}
           style={{ opacity: "0" }} // Set initial opacity to 0
           onLoad={(e) => {
@@ -508,26 +512,30 @@ export default function Id({ item }) {
           }}
         ></img>
 
-        <div
-          onClick={() => {
-            product.img.length - 1 === imageIndex
-              ? setImageIndex(0)
-              : setImageIndex(imageIndex + 1);
-          }}
-          className="heartbeat absolute transition-all duration-150 p-3 cursor-pointer active:scale-95 hover:scale-105 group-hover:opacity-100 opacity-30 rounded hover:bg-neutral-900 bg-neutral-900/70 text-white top-[45%] sm:right-5 right-2 "
-        >
-          {">"}
-        </div>
-        <div
-          onClick={() => {
-            imageIndex === 0
-              ? setImageIndex(product.img.length - 1)
-              : setImageIndex(imageIndex - 1);
-          }}
-          className="heartbeat absolute transition-all duration-150 cursor-pointer p-3 active:scale-95 hover:scale-105 group-hover:opacity-100 opacity-30 rounded hover:bg-neutral-900 bg-neutral-900/70 text-white top-[45%] sm:left-5 left-2"
-        >
-          {"<"}
-        </div>
+        {product.img.length > 1 && (
+          <div
+            onClick={() => {
+              product.img.length - 1 === imageIndex
+                ? setImageIndex(0)
+                : setImageIndex(imageIndex + 1);
+            }}
+            className="heartbeat absolute transition-all duration-150 p-6 cursor-pointer active:scale-95 hover:scale-105 group-hover:opacity-100 opacity-30 rounded hover:bg-neutral-900 bg-neutral-900/70 text-white top-[45%] sm:right-5 right-2 "
+          >
+            {">"}
+          </div>
+        )}
+        {product.img.length > 1 && (
+          <div
+            onClick={() => {
+              imageIndex === 0
+                ? setImageIndex(product.img.length - 1)
+                : setImageIndex(imageIndex - 1);
+            }}
+            className="heartbeat absolute transition-all duration-150 cursor-pointer p-6 active:scale-95 hover:scale-105 group-hover:opacity-100 opacity-30 rounded hover:bg-neutral-900 bg-neutral-900/70 text-white top-[45%] sm:left-5 left-2"
+          >
+            {"<"}
+          </div>
+        )}
       </>
     );
   }
