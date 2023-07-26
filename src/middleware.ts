e6c8268fec1d403352ef5f8d51bad8c37c84dc31
Nico Horn <contact@nicohorn.com>
2023-07-26 20:21:41 -0300

    fixed admin error

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,14 +3,15 @@ import { getSession } from "@auth0/nextjs-auth0";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-import { sign, verify } from "jsonwebtoken";
-import { BufferSource } from "stream/web";
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest, response: NextResponse) {
-  if (
-    request.cookies.get("userId").value !==
-    (process.env.NEXT_PUBLIC_ADMIN1 || process.env.NEXT_PUBLIC_ADMIN2)
+  if (request.cookies.get("userId").value === process.env.NEXT_PUBLIC_ADMIN1) {
+    return NextResponse.next();
+  } else if (
+    request.cookies.get("userId").value === process.env.NEXT_PUBLIC_ADMIN2
   ) {
+    return NextResponse.next();
+  } else {
     return NextResponse.redirect(new URL("/", request.url));
   }
 }
