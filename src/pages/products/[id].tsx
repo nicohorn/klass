8c26a397bc19908509a1b4f6d222f5323c907aff
Nico Horn <50425596+nicohorn@users.noreply.github.com>
2023-08-02 20:21:03 -0300

    Update [id].tsx

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -169,6 +169,6 @@ export async function getStaticPaths() {
 
   return {
     paths,
-    fallback: true, // See the "fallback" section below
+    fallback: false, // See the "fallback" section below
   };
 }
